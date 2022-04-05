43ddc92487f16f655cc9aab13b289706509aa407
Dola9 <https://gitlab.com/Dola9>
2022-04-05 15:34:45 +0200

    install packages and structure of project

---
+++ lib/screens/auth/login/screen/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const routeName = '/';
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/utils/routes.dart
@@ -0,0 +1,4 @@
+
+import 'package:dukkantek/screens/auth/login/screen/login_screen.dart';
+
+var routes = {LoginScreen.routeName: (context) => const LoginScreen()};
